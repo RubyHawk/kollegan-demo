c5b128d965188d884d1584e0982b68a57359bcf4
RubyHawk <132721829+RubyHawk@users.noreply.github.com>
2026-06-14 18:35:54 +0200

    Refine Fluffy homepage art direction (#687)

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -99,44 +99,53 @@ export default async function PublicHomePage() {
           </div>
 
           <div className="fluffy-collage fluffy-rise fluffy-delay-1" aria-label="Fluffy's menybilder">
-            <figure className="fluffy-menu-ticket fluffy-menu-ticket--taco">
-              <figcaption>
-                <span>19</span>
-                <strong>Tacokebab</strong>
-                <small>Välj mellan kebab / gyros / kyckling</small>
-                <em>89 / 149 / 239</em>
-              </figcaption>
-              <span className="fluffy-menu-ticket__photo" aria-hidden="true">
+            <div className="fluffy-collage-board">
+              <div className="fluffy-board-heading">
+                <span>Dagens</span>
+                <strong>Pizzor &amp; subs</strong>
+                <small>Snabbt stopp i Laxå</small>
+              </div>
+              <div className="fluffy-board-clippings">
+                <figure className="fluffy-menu-ticket fluffy-menu-ticket--taco">
+                  <figcaption>
+                    <span>19</span>
+                    <strong>Tacokebab</strong>
+                    <small>Välj mellan kebab / gyros / kyckling</small>
+                    <em>89 / 149 / 239</em>
+                  </figcaption>
+                  <span className="fluffy-menu-ticket__photo" aria-hidden="true">
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img src="/fluffys/menu/pizza-kebab-board.jpg" alt="" />
+                  </span>
+                </figure>
+                <figure className="fluffy-menu-ticket fluffy-menu-ticket--mix">
+                  <figcaption>
+                    <span>21</span>
+                    <strong>Pick&apos;n mix kebaben</strong>
+                    <small>Sallad, tomat, gurka, feferoni, lök, sås</small>
+                    <em>89 / 149 / 239</em>
+                  </figcaption>
+                  <span className="fluffy-menu-ticket__photo" aria-hidden="true">
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img src="/fluffys/menu/pizza-kebab-board.jpg" alt="" />
+                  </span>
+                </figure>
+              </div>
+              <div className="fluffy-collage-sign" aria-hidden="true">
+                <span>Subs</span>
                 {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src="/fluffys/menu/pizza-kebab-board.jpg" alt="" />
-              </span>
-            </figure>
-            <figure className="fluffy-menu-ticket fluffy-menu-ticket--mix">
-              <figcaption>
-                <span>21</span>
-                <strong>Pick&apos;n mix kebaben</strong>
-                <small>Sallad, tomat, gurka, feferoni, lök, sås</small>
-                <em>89 / 149 / 239</em>
-              </figcaption>
-              <span className="fluffy-menu-ticket__photo" aria-hidden="true">
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src="/fluffys/menu/pizza-kebab-board.jpg" alt="" />
-              </span>
-            </figure>
-            <div className="fluffy-collage-sign" aria-hidden="true">
-              <span>Subs</span>
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img src="/fluffys/favicon.svg" alt="" />
-              <span>Pizza</span>
+                <img src="/fluffys/favicon.svg" alt="" />
+                <span>Pizza</span>
+              </div>
+              <figure className="fluffy-gluten-ticket">
+                <span aria-hidden="true">Gluten free</span>
+                <figcaption>
+                  <strong>Glutenfritt</strong>
+                  <em>189:-</em>
+                  <small>Samma goda pizzor, nu även på glutenfri botten.</small>
+                </figcaption>
+              </figure>
             </div>
-            <figure className="fluffy-gluten-ticket">
-              <span aria-hidden="true">Gluten free</span>
-              <figcaption>
-                <strong>Glutenfritt</strong>
-                <em>189:-</em>
-                <small>Samma goda pizzor, nu även på glutenfri botten.</small>
-              </figcaption>
-            </figure>
             <figure className="fluffy-sub-ticket">
               <figcaption>
                 <strong>Italian duo</strong>
